25c28cb7f7748e6a6f0f51370573121110429d88
GOVERNET\reubenellis <rellis@governet.net>
2017-11-28 16:53:25 -0700

    added a sortPackages function

--- src/index.js
+++ src/index.js
@@ -18,5 +18,21 @@ module.exports = {
       const errorMessage = 'The input is not an array of strings';
       return errorMessage;
     }
+  },
+
+  sortPackages(packages = {}) {
+    const noDependencies = _.pick(packages, _.isEmpty);
+    const withDependencies = _.omit(packages, _.isEmpty);
+    const orderedList = [..._.keys(noDependencies)];
+
+    while (_.any(withDependencies)) {
+      for (const library in withDependencies) {
+        if (_.contains(orderedList, withDependencies[library])) {
+          orderedList.push(library);
+          delete withDependencies[library];
+        }
+      }
+    }
+    return orderedList;
   }
 }
