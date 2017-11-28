47e95025d7559ba85809dda5d255d3b1f9c61a1a
GOVERNET\reubenellis <rellis@governet.net>
2017-11-28 16:46:55 -0700

    added test for sortPackages

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 let _ = require('underscore');
-let DelimeterValue = ': ';
+let DelimiterValue = ': ';
 
 module.exports = {
   convertArrayToObject(libraryFiles = []) {
-    return libraryFiles;
+    if (Array.isArray(libraryFiles)) {
+      return _.reduce(libraryFiles, (dependencyList, library) => {
+        const [key, value] = library.split(DelimiterValue);
+        if (value === undefined) {
+          const missingValuesErrorMessage = 'Not all array records were in the correct format conversion not successful';
+          return missingValuesErrorMessage;
+        } else {
+          dependencyList[key] = value;
+          return dependencyList;
+        }
+      }, {});
+    } else {
+      const errorMessage = 'The input is not an array of strings';
+      return errorMessage;
+    }
   }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -3,22 +3,42 @@ const expect = chai.expect;
 const index = require('./index.js');
 
 
-describe('Test to verify the array can convert into an object', () => {
+describe('convertArrayToObject', () => {
   it('turns an array into an object', () => {
 
-    const correctInputFormat = `[
-        'react-dom: react',
-        'react: ',
-        'eslint-config-standard: eslint'],
-        'eslint: '`;
+    const correctInputFormat = ['react-dom: react', 'react: ', 'eslint-config-standard: eslint', 'eslint: '];
 
     const convertedObject = index.convertArrayToObject(correctInputFormat);
 
-    expect(convertedObject).to.deep.eq({
-      'react-dom': 'react',
-      react: '',
-      'eslint-config-standard': 'eslint',
-      eslint: ''
-    })
-  })
+    expect(convertedObject).to.deep.equal({
+      'react-dom': 'react', react: '', 'eslint-config-standard': 'eslint', eslint: ''
+    });
+  });
+  it('Displays an error message stating the input is not an array of strings', () => {
+
+    // const incorrectInputFormat = ['react-domreact', 'react: ', 'eslint-config-standard:eslint', 'eslint: '];
+    const incorrectInput = 'This is Not An Array Of Strings';
+    const convertedObject = index.convertArrayToObject(incorrectInput);
+    const errorMessage = 'The input is not an array of strings';
+
+    expect(convertedObject).to.equal(errorMessage);
+  });
+  it('Displays an error message stating that not all array records were in the correct format', () => {
+
+    const incorrectInputFormat = ['react-dom: react', 'react: ', 'eslint-config-standard:eslint', 'eslint'];
+    const convertedObject = index.convertArrayToObject(incorrectInputFormat);
+    const errorMessage = 'Not all array records were in the correct format conversion not successful';
+    expect(convertedObject).to.equal(errorMessage);
+  });
+});
+
+describe('sortPackages', () => {
+  it('orders packages with dependencies first', () => {
+
+    const packages = ['react-dom: react', 'react: ', 'eslint-config-standard: eslint', 'eslint: '];
+
+    expect(index.sortPackages(packages)).to.deep.equal({
+       react: '', 'react-dom': 'react', eslint: '', 'eslint-config-standard': 'eslint'
+    });
+  });
 });
